b00a8b10f051f361ce18b0d6c11830eb2b8acd86
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2022-02-07 16:55:49 +0400

    feat: protected route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,12 @@ export class AuthController {
     @UseGuards(AuthGuard('local'))
     @Post('login')
     async login(@Request() req) {
+        return this.authService.login(req.user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
         return req.user;
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) { }
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService
+    ) { }
 
     async validateUser(username: string, password: string): Promise<any> {
         const user = await this.usersService.findOne(username);
@@ -13,5 +17,12 @@ export class AuthService {
             return result;
         }
         return null;
+    }
+
+    async login(user: any) {
+        const payload = { username: user._doc.username, sub: user._doc._id };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
     }
 }
